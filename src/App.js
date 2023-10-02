6eb221ebe90b466835b2655b84567142014cac82
Uranchimeg Jamsran <uranchimeg@mezorn.com>
2023-10-02 11:46:55 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
 
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'increment':
+			state.count = state.count + 1;
+			return { count: state.count };
+		case 'decrement':
+			state.count = state.count - 1;
+			return { count: state.count };
+		default:
+			return state;
+	}
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<div className='App'>
+			<div>
+				<h1>Count: {state.count}</h1>
+				<button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+				<button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
